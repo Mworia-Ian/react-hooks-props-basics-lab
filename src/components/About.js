f587455a2c57e9cad7aae7364fb969c29b6794cf
Ian Mworia <ianmworia156@gmail.com>
2024-04-29 22:23:57 +0300

    Lab Completed

--- src/components/About.js
+++ src/components/About.js
@@ -3,15 +3,15 @@ import Links from "./Links";
 import user from "../data/user";
 
 
-function About(props) {
-  if(!props.bio){
+function About({bio}) {
+  if(!bio){
     return null
   }else{
 
     return (
       <div id="about">
         <h2>About Me</h2>
-        <p>{props.bio}</p>
+        <p>{bio}</p>
         <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
         <Links github={"https://github.com/liza"}  linkedin={"https://www.linkedin.com/in/liza/"} />
       </div>

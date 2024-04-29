7a3af53c4a55867449d86479cec5152b72544856
Ian Mworia <ianmworia156@gmail.com>
2024-04-29 22:17:03 +0300

    Lab Completed

--- src/components/About.js
+++ src/components/About.js
@@ -13,8 +13,7 @@ function About(props) {
         <h2>About Me</h2>
         <p>{props.bio}</p>
         <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-        <Links github={"https://github.com/liza"} />
-        <Links linkedin={"https://www.linkedin.com/in/liza/"} />
+        <Links github={"https://github.com/liza"}  linkedin={"https://www.linkedin.com/in/liza/"} />
       </div>
     );
   }
